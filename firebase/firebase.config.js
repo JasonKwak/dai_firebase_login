5bb5bcb096b50a96345c1b3dc1f73dc83b438c00
Jason Kwak <jasonkwak@d207-023-163-244.wireless.sfu.ca>
2022-11-08 18:19:48 -0800

    initial commit

---
+++ firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+  appId: process.env.appId
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
